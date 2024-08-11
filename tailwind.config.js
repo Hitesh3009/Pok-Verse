7d72e2a277e93bd52f45607258fb419dfb3b339c
Hitesh3009 <hiteshbhosle3009@gmail.com>
2024-08-11 15:32:08 +0530

    `Revert "added the slidetoleft and slidetobottom animation for the specific pokemon details page by configuring the tailwind config.js"
    This reverts commit bdf64064da8787d6c65c9fa6781c52ac13cff945.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const { transform } = require('next/dist/build/swc');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -9,41 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      // backgroundImage: {
-      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      //   "gradient-conic":
-      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      // },
-      // keyframes: {
-      //   slideToLeft: {
-      //     wiggle: {
-      //       from: { translateX: '0' },
-      //       to: { translateX: '-100%' }
-      //     }
-      //   }
-      // },
-      keyframes: {
-        slideToLeft: {
-          '0%':{
-            transform:'translateX(400%)'
-          },
-          '100%':{
-            transform:'translateX(-10%)'
-          }
-        },
-        slideToBottom: {
-          '0%':{
-            transform:'translateY(-100%)'
-          },
-          '100%':{
-            transform:'translateY(5%)'
-          }
-        },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      animation: {
-        slideToLeft: 'slideToLeft 1.5s linear 1',
-        slideToBottom: 'slideToBottom 1.5s linear 1',
-      }
     },
   },
   plugins: [
