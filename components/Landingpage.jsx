a30dc6ca271cd413350332bb6fc7cb860318230d
Hitesh3009 <hiteshbhosle3009@gmail.com>
2024-08-24 22:03:54 +0530

    removed the region part

--- components/Landingpage.jsx
+++ components/Landingpage.jsx
@@ -5,7 +5,6 @@ const LandingPage = () => {
   return (
     <div className=''>
         <Link href='/allpokemons/0'><button className='bg-black text-white px-3 py-2 rounded-md'>Go to all pokemons page</button></Link>
-        <Link href='/pokedex/regions'><button className='bg-black text-white px-3 py-2 rounded-md'>Pokemons by Region</button></Link>
     </div>
   )
 }
