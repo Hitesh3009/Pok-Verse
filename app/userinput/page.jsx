398e9f61e2df574f608f30165a4efaa6a6ec465b
Hitesh3009 <hiteshbhosle3009@gmail.com>
2024-08-28 14:54:50 +0530

    changed the text color for the search pokemon text

--- app/userinput/page.jsx
+++ app/userinput/page.jsx
@@ -25,7 +25,7 @@ const UserInput = ({ totalRes }) => {
             <div className={`searchPokemon flex flex-col items-center space-y-4 my-7 ${(!Number.isInteger(page) || page < 0 || page > pageCount) && 'hidden'}`} >
 
                 {/* Label to display the field name */}
-                <label htmlFor="pokemonName" className='text-3xl text-center font-bold'>Search Pokemon</label>
+                <label htmlFor="pokemonName" className='text-3xl text-center font-bold text-white'>Search Pokemon</label>
 
                 {/* Input field for the user to enter the pokemon name */}
                 <input type="text" name="pokemonName" id="pokemonName" aria-placeholder='Search Pokemon' className='outline-none border-2 border-b-black w-72 sm:w-80 md:w-96 pl-3 focus:transition focus:border-[3px] focus:border-b-red-400 focus:shadow-lg focus:shadow-yellow-300 rounded-md delay-300 h-9' value={userInp} onChange={handleUserInp} />
